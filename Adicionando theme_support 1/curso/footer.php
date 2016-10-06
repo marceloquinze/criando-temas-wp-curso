c8530ede9058b2d8cc49a74abfc8dcd853087f4c
marceloquinze <marceloquinze@gmail.com>
2016-10-06 19:40:11 -0300

    Aula 16 (Udemy)

---
+++ Adicionando theme_support 1/curso/footer.php
@@ -0,0 +1,11 @@
+<footer class="container">
+	<div class="row">
+		<div class="copyright col-md-7"><p>Copyright</p></div>
+		<nav class="menu-rodape col-md-5 text-right">
+			<?php wp_nav_menu( array('theme-location' => 'menu-rodape') ); ?>
+		</nav>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>

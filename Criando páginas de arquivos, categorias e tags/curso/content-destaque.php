bc123ac8b41268af557d47b36b60f972879cea84
marceloquinze <marceloquinze@gmail.com>
2016-10-06 20:02:33 -0300

    Aula 30 (Udemy)

---
+++ Criando páginas de arquivos, categorias e tags/curso/content-destaque.php
@@ -0,0 +1,9 @@
+<article class="noticia-destaque">
+	<div class="miniatura">
+	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?></a>		
+	</div>
+	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	<p>por <span><?php the_author_posts_link(); ?></span> em <span><?php the_category(' '); ?></span> <?php the_tags('Tags: ', ', '); ?></p>
+	<p><?php echo get_the_date(); ?></p>
+	<p><?php the_excerpt(); ?></p>
+</article>
---
+++ Criando páginas de arquivos, categorias e tags/curso/searchform.php
@@ -0,0 +1,3 @@
+<form role="search" method="get" action="<?php echo home_url('/'); ?>">
+	<input type="search" class="form-control" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s" title="Pesquisa" />
+</form>

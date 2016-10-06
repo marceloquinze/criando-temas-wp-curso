dfd4434722327b6e4aa48ef80e4f1f293fd7089c
marceloquinze <marceloquinze@gmail.com>
2016-10-06 19:42:37 -0300

    Aula 17 (Udemy)

---
+++ Adicionando theme_support 2/curso/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso WordPress</title>
+
+	<?php wp_head(); ?>
+	
+</head>
+<body <?php body_class(); ?>>
+
+
+<header>
+	<div class="barra-topo container">
+	<div class="row">
+		<div class="redes-sociais col-md-4">Redes Sociais</div>
+		<div class="pesquisa col-md-8">Pesquisa</div>
+	</div>
+
+	</div>
+
+	<div class="area-menu container">
+		<div class="row">
+			<div class="logo col-md-3">Logo</div>
+			<div class="menu-principal col-md-9 text-right">
+				<?php wp_nav_menu( array( 'theme-location' => 'meu_menu_principal') ); ?>
+			</div>
+		</div>
+	</div>
+	
+</header>
---
+++ Adicionando theme_support 2/curso/page-home.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="conteudo">
+	<main>
+		<section class="slide container">Slide</section>
+		<section class="servicos container">Serviços</section>
+		<section class="meio container">
+			<div class="row">
+				<aside class="barra-lateral col-md-3">Barra Lateral</aside>
+				<div class="noticias col-md-9">
+				<p>Esta será a área de notícias da página home</p>
+
+				</div>
+			</div>
+		</section>
+		<section class="mapa container">Mapa</section>
+	</main>	
+</div>
+<?php get_footer(); ?>

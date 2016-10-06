293a8fd15317a29a15b213727a455db64fd77d8c
marceloquinze <marceloquinze@gmail.com>
2016-10-06 19:33:10 -0300

    Aulas 10 e 11 (Udemy)

---
+++ Adicionando menus/curso/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div class="conteudo">
+	<main>
+		<section class="slide">Slide</section>
+		<section class="servicos">Serviços</section>
+		<section class="meio">
+			
+			<aside class="barra-lateral">Barra Lateral</aside>
+			<div class="noticias">Noticias</div>
+		</section>
+		<section class="mapa">Mapa</section>
+	</main>	
+</div>
+<?php get_footer(); ?>

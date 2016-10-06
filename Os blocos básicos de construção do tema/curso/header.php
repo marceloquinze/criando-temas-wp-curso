5ca845e39f9ea62d42adfa0eedf3002d5d0417f2
marceloquinze <marceloquinze@gmail.com>
2016-10-06 19:25:22 -0300

    Aulas 6 e 7 (Udemy)

---
+++ Os blocos básicos de construção do tema/curso/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso WordPress</title>
+</head>
+<body <?php body_class(); ?>>
+	<p>Este é um parágrafo</p>
+
+<header>
+	<div class="barra-topo">
+		<div class="redes-sociais"></div>
+		<div class="pesquisa"></div>
+
+	</div>
+
+	<div class="area-menu">
+		<div class="logo"></div>
+		<div class="menu"></div>
+	</div>
+	
+</header>
---
+++ Os blocos básicos de construção do tema/curso/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div class="conteudo">
+	<main>
+		<section class="slide"></section>
+		<section class="servicos"></section>
+		<section class="meio">
+			
+			<aside class="barra-lateral"></aside>
+			<div class="noticias"></div>
+		</section>
+		<section class="mapa"></section>
+	</main>	
+</div>
+<?php get_footer(); ?>

7315c7fdb738448881cee8a39ecebe0d1ed7f5b1
marceloquinze <marceloquinze@gmail.com>
2016-10-06 22:29:51 -0300

    Aula 44 (Udemy)

---
+++ Criando nossos próprios shortcodes/curso/footer.php
@@ -0,0 +1,13 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="copyright col-md-7 col-sm-7 col-xs-4"><p>Copyright</p></div>
+			<nav class="menu-rodape col-md-5 col-sm-5 col-xs-8 text-right">
+				<?php wp_nav_menu( array('theme-location' => 'menu-rodape') ); ?>
+			</nav>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>

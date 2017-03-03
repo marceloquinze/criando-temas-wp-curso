0b39676673dcb4bfe04b69db4592bddf29a8766a
Marcelo Vieira <marceloquinze@gmail.com>
2017-03-03 12:25:47 -0300

    Aula 50, 51 e 52 (Udemy) - adição de pre_get_posts()

--- Theme Customizer 3, 4 e 5/curso/functions.php
+++ Theme Customizer 3, 4 e 5/curso/functions.php
@@ -68,6 +68,20 @@ if (function_exists('register_sidebar')){
 	);
 }
 
+// Alterar o número de itens por página no blog
+
+function num_itens_blog( $query ){
+	if( is_admin() || ! $query->is_main_query() )
+	return;
+
+	// Página blog
+	if ( is_home() ){
+		$query->set( 'posts_per_page', 2 );
+		return;
+	}
+}
+
+add_action( 'pre_get_posts', 'num_itens_blog', 1 );
 
 // Shortcodes para mostrar o telefone na página de contato
 

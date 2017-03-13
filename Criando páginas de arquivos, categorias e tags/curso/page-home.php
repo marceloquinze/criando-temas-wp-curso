cc0b6c9b1b3272ba55b455431e03c654b9bb3130
Marcelo Vieira <marceloquinze@gmail.com>
2017-03-13 18:56:41 -0300

    Aulas 32 (Udemy) - sugestão Giberto Medeiros

--- Criando páginas de arquivos, categorias e tags/curso/page-home.php
+++ Criando páginas de arquivos, categorias e tags/curso/page-home.php
@@ -15,25 +15,34 @@
 					</aside>
 					<div class="noticias col-md-9">
 						<div class="row">
+
 						<?php 
 
-						// Quais categorias serão inclusas na nossa listagem?
-						$itens = get_categories(array(6,7,8));
-						//echo '<pre>';
-						//print_r($itens);
-						//echo '</pre>';
+						/**
+						 * Variáveis para divs das notícias e arquivo de conteúdo
+						 *
+						 * Sugestão Gilberto Medeiros
+						 * @see https://www.udemy.com/criando-temas-customizados-com-wordpress/learn/v4/questions/1968272
+						 *
+						 */
 
-						// Começamos um contador para cada item buscado. No caso, três itens
-						$cont = 1;
+						$tamanho = 'col-md-12';
+                        			$op_content = 'destaque';
 
-						// Para cada categoria achada, buscamos um só item dessa categoria
-						// Este loop roda até o contador valer 3
+                        			// Quais categorias serão inclusas na nossa listagem?
+						$itens = get_categories(array('include' => '6,7,8'));
+
+						// echo '<pre>';
+						// print_r($itens);
+						// echo '</pre>';
+
+						// Para cada categoria encontrada, buscamos um só item dessa categoria
 						foreach($itens as $item):
 
 							// Pegamos cada item dentro do loop para compor nossa consulta
 							$args = array(
-								'category__in'		=> $item->term_id,
-								'posts_per_page'	=> 1
+								'category__in' => $item->cat_ID,
+								'posts_per_page' => 1
 							);
 
 							// Criação do objeto da classe WP_Query
@@ -44,36 +53,31 @@
 								while($consulta->have_posts()):
 									$consulta->the_post();
 
-								// Temos que ver se o contador vale 1. Se valer, vai em frente e mostra nosso template de post com 12 colunas
-								// Se não valer 1, chama outro template, envolto com 6 colunas
-								if($cont == 1):
-						?>
+							// Abaixo, usamos as variáveis $tamanho e $op_content
+							// Na primeira repetição do loop, elas têm o valor indicado acima. Na segunda e terceira, mudam de valor
+							?>
+								<div class="<?php echo $tamanho; ?>">
+									<?php get_template_part('content', $op_content); ?>
+								</div>								
 
-							<div class="col-md-12">
-								<?php get_template_part('content', 'destaque'); ?>
-							</div>
+							<?php
 
-							<?php else: ?>
+								// Reiniciamos o valor das variáveis $tamanho e $op_content com novos valores
+								// Esses novos valores só valerão do segundo loop em diante
 
-							<div class="col-md-6">
-								<?php get_template_part('content', 'secundaria'); ?>
-							</div>							
-
-						<?php
-								endif;
+								$tamanho = 'col-md-6';
+	                        				$op_content = 'secundaria';
 
 								endwhile;
-								// Reseta a conculta a cada passo do loop
+								// Reseta a consulta a cada passo do loop
 								wp_reset_postdata();
 
 							endif;
 
-						// Incrementa o contador antes de acabar o loop
-						$cont++;
-
+						// Fim do loop foreach
 						endforeach;
 
-						?>
+						?>							
 							
 						</div>
 					</div>

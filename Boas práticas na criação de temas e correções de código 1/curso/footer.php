b8f46ef0a08ab6389cb58b6e343f0b13a2c8e976
Marcelo Vieira <marceloquinze@gmail.com>
2017-07-04 10:22:51 -0300

    Aulas 36 (Udemy) - Correção theme_location

--- Boas práticas na criação de temas e correções de código 1/curso/footer.php
+++ Boas práticas na criação de temas e correções de código 1/curso/footer.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="copyright col-md-7"><p>Copyright</p></div>
 			<nav class="menu-rodape col-md-5 text-right">
-				<?php wp_nav_menu( array('theme-location' => 'menu-rodape') ); ?>
+				<?php wp_nav_menu( array('theme_location' => 'menu_rodape') ); ?>
 			</nav>
 		</div>
 	</div>

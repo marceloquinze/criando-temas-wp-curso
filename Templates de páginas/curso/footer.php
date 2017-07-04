bb70c83b1d84cb30b79e0c149681645f6d139ede
Marcelo Vieira <marceloquinze@gmail.com>
2017-07-04 10:08:19 -0300

    Aulas 16 e 17 (Udemy) - Correção theme_location

--- Templates de páginas/curso/footer.php
+++ Templates de páginas/curso/footer.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="copyright col-md-7"><p>Copyright</p></div>
 		<nav class="menu-rodape col-md-5 text-right">
-			<?php wp_nav_menu( array('theme_location' => 'menu-rodape') ); ?>
+			<?php wp_nav_menu( array('theme_location' => 'menu_rodape') ); ?>
 		</nav>
 	</div>
 </footer>

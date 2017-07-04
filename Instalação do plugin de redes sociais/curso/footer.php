5103de87de7c3af9090631b8702020c55ecb55fa
Marcelo Vieira <marceloquinze@gmail.com>
2017-07-04 10:28:51 -0300

    Aula 45 (Udemy) - Correção theme_location

--- Instalação do plugin de redes sociais/curso/footer.php
+++ Instalação do plugin de redes sociais/curso/footer.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="copyright col-md-7 col-sm-7 col-xs-4"><p>Copyright</p></div>
 			<nav class="menu-rodape col-md-5 col-sm-5 col-xs-8 text-right">
-				<?php wp_nav_menu( array('theme-location' => 'menu-rodape') ); ?>
+				<?php wp_nav_menu( array('theme_location' => 'menu_rodape') ); ?>
 			</nav>
 		</div>
 	</div>
